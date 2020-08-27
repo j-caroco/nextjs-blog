d771375294973b99a61a2c297cff9acf2e2e734e
j-caroco <j.caroco@xgeeks.io>
2020-08-27 10:46:09 +0100

    client side pagination

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,45 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
 
-export default function Home({ allPostsData }) {
+
+import axios from 'axios'
+import MUIDataTable from "mui-datatables"
+import { getData } from '../lib/data';
+
+const columns = ["id", "created_at", "entity", "amount", "type", "source"];
+ 
+const data = [
+ ["Joe James", "Test Corp", "Yonkers", "NY"],
+ ["John Walsh", "Test Corp", "Hartford", "CT"],
+ ["Bob Herm", "Test Corp", "Tampa", "FL"],
+ ["James Houston", "Test Corp", "Dallas", "TX"],
+];
+const options = {
+  filterType: 'checkbox',
+};
+
+
+export default function Home({ names }) {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-            <Link href="/posts/[id]" as={`/posts/${id}`}>
-              <a>{title}</a>
-            </Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
+    <MUIDataTable 
+      title={"Random data"} 
+      data={names} 
+      columns={columns} 
+      options={options} 
+    />
   )
 }
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
+  const names = [];
+  const data = await getData();
+
+  data.forEach(element => names.push(Object.values(element)) );
+
+  console.log(names);
+  return {props: {
+    names
+  }}
+
 }
+
+
+

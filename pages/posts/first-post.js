b4407e9abbbd77a78edea8b14f95d5894ac1e1c4
Luís Paulo Serra Ferreira <l.ferreira@xgeeks.io>
2020-08-27 13:54:14 +0100

    Fix initial value

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,23 @@
-import { getSingleData } from "../../lib/data";
+import { getSingleData, getNumberOfItems } from "../../lib/data";
 import Pagination from "react-js-pagination";
 import React, { Component } from "react";
 
+export async function getStaticProps() {
+	const singleData = await getSingleData(1);
+	const numberOfItems = await getNumberOfItems();
+	return {
+		props: { singleData, numberOfItems },
+	};
+}
 export default class FirstPost extends Component {
 	constructor(props) {
 		super(props);
+		console.log(props.numberOfItems);
+
 		this.state = {
-			activePage: 15,
-			info: [],
+			activePage: 1,
+			info: props.singleData,
+			total: props.numberOfItems,
 		};
 	}
 
@@ -21,22 +31,26 @@ export default class FirstPost extends Component {
 		return (
 			<div>
 				<table>
-					<tr>
-						<th>UserID</th>
-						<th>ID</th>
-						<th>Title</th>
-						<th>Body</th>
-					</tr>
-					<tr key={this.state.info.userId}>
-						<td>{this.state.info.id}</td>
-						<td>{this.state.info.title}</td>
-						<td>{this.state.info.body}</td>
-					</tr>
+					<thead>
+						<tr>
+							<th>UserID</th>
+							<th>ID</th>
+							<th>Title</th>
+							<th>Body</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr key={this.state.info.userId}>
+							<td>{this.state.info.id}</td>
+							<td>{this.state.info.title}</td>
+							<td>{this.state.info.body}</td>
+						</tr>
+					</tbody>
 				</table>
 				<Pagination
 					activePage={this.state.activePage}
-					itemsCountPerPage={10}
-					totalItemsCount={450}
+					itemsCountPerPage={1}
+					totalItemsCount={this.state.total}
 					pageRangeDisplayed={5}
 					onChange={this.handlePageChange.bind(this)}
 				/>
